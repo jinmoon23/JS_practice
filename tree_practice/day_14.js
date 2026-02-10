988079e87cfb836781121f572d00eeb071d95a0f
jinmun23 <wlsansdlek1@me.com>
2026-02-10 12:13:30 +0900

    practice: day_14

---
+++ tree_practice/day_14.js
@@ -0,0 +1,34 @@
+function preorder(nodes, idx = 0, result = []) {
+  if (idx >= nodes.length) return result;
+
+  result.push(nodes[idx]);
+  preorder(nodes, idx * 2 + 1, result);
+  preorder(nodes, idx * 2 + 2, result);
+
+  return result;
+}
+
+function inorder(nodes, idx = 0, result = []) {
+  if (idx >= nodes.length) return result;
+
+  inorder(nodes, idx * 2 + 1, result);
+  result.push(nodes[idx]);
+  inorder(nodes, idx * 2 + 2, result);
+
+  return result;
+} 
+
+
+function postorder(nodes, idx = 0, result = []) {
+  if (idx >= nodes.length) return result;
+
+  postorder(nodes, idx * 2 + 1, result);
+  postorder(nodes, idx * 2 + 2, result);
+  result.push(nodes[idx]);
+
+  return result;
+}
+
+console.log(preorder([1,2,3,4,5,6,7]));
+console.log(inorder([1,2,3,4,5,6,7]));
+console.log(postorder([1,2,3,4,5,6,7]));
