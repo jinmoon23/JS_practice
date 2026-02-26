b3dbb3c036777637dd2dc8b9182467813d0f3e07
jinmun23 <wlsansdlek1@me.com>
2026-02-26 09:21:02 +0900

    pracitce: day_22

---
+++ combinations_practice/day_22.js
@@ -0,0 +1,13 @@
+function combinations(arr, r) {
+  if (r === 0) return [[]];
+  if (arr.length === 0 || r > arr.length) return [];
+
+  const [first, ...rest] = arr;
+
+  const withoutFirst = combinations(rest, r);
+  const withFirst = combinations(rest, r - 1).map((comb) => [first, ...comb]);
+
+  return [...withoutFirst, ...withFirst];
+}
+
+console.log(combinations(['a','b','c','d','e'], 2));
