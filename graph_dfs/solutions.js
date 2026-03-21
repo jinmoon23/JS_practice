15212e9e8fdbf7279b00c0c573bbfeaca8ed85aa
jinmun23 <wlsansdlek1@me.com>
2026-03-21 20:36:39 +0900

    practice: graph_dfs

---
+++ graph_dfs/solutions.js
@@ -0,0 +1,28 @@
+function solutions(graph, start) {
+  const connections = {};
+  for (const [prev, next] of graph) {
+    if (!connections[prev]) connections[prev] = [next];
+    else connections[prev].push(next);
+  }
+  console.log(connections);
+
+  function dfs(node, visited, result) {
+    visited.add(node);
+    result.push(node);
+    // 핵심 -> 리프 노드의 경우 인접 노드가 없으므로 connections[node]에서 undefined를 반환하여 에러가 발생한다.
+    // 이를 방지하기 위해 || []를 하는것.
+    (connections[node] || []).forEach((neighbor) => {
+      if (!visited.has(neighbor)) {
+        dfs(neighbor, visited, result);
+      }
+    })
+  }
+  const visited = new Set();
+  const result = [];
+
+  dfs(start, visited, result);
+  return result;
+}
+
+console.log(solutions([['A', 'B'], ['B', 'C'], ['C', 'D'], ['D', 'E']], 'A'));
+console.log(solutions([['A', 'B'], ['A', 'C'], ['B', 'D'], ['B', 'E'], ['C', 'F'], ['E', 'F']], 'A'));
