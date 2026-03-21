8ab897e44f93917befca365446a258a97764c9aa
jinmun23 <wlsansdlek1@me.com>
2026-03-22 07:04:53 +0900

    practice: graph_dfs day2

---
+++ graph_dfs/day_2.js
@@ -0,0 +1,31 @@
+function solutions(graph, start) {
+  // 1. graph 배열을 object로 변환
+  const connections = {};
+  for (const [curr, next] of graph) {
+    if (!connections[curr]) connections[curr] = [next];
+    connections[curr].push(next);
+  }
+
+  function dfs(node, visited, result) {
+    visited.add(node); result.push(node);
+
+    const neighbors = connections[node] || [];
+    
+    for (const neighbor of neighbors) {
+      if (!visited.has(neighbor)) {
+        dfs(neighbor, visited, result);
+      }
+    }
+  }
+
+  const visited = new Set();
+  const result = [];
+
+  dfs(start, visited, result);
+
+  return result;
+}
+
+
+console.log(solutions([['A', 'B'], ['B', 'C'], ['C', 'D'], ['D', 'E']], 'A'));
+console.log(solutions([['A', 'B'], ['A', 'C'], ['B', 'D'], ['B', 'E'], ['C', 'F'], ['E', 'F']], 'A'));
